cc98c8f52fc0cba72d6b2c3db6c87f6225ec6028
Wesley <wesaboo@gmail.com>
2020-09-01 19:05:41 -0700

    adds dynamic date on footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,29 @@
 import React from "react";
 
 export default function Footer() {
+  const getYear = () => new Date().getFullYear();
+
   return (
-    <footer className="footer" style={{position: "absolute", bottom: "0", width: "100%", height: "40px",lineHeight: "60px", backgroundColor: "#f5f5f5"}}>
-      <div className="container" style={{width: "auto", maxWidth: "680px"}}>
-        <span className="text-muted">Place sticky footer content here.</span>
+    <footer
+      className="footer"
+      style={{
+        position: "fixed",
+        bottom: "0",
+        width: "100%",
+        height: "40px",
+        backgroundColor: "#f5f5f5",
+      }}
+    >
+      <div
+        className="container"
+        style={{
+          width: "auto",
+          maxWidth: "680px",
+          margin: "auto",
+          textAlign: "center",
+        }}
+      >
+        <span className="text-muted">Copyright &#169; {getYear()}</span>
       </div>
     </footer>
   );
