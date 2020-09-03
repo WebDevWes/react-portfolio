50db07902fd0f3d64e045cb1d947e0fe9c1fccb2
Wesley <wesaboo@gmail.com>
2020-09-03 14:57:27 -0700

    created wrapper and pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Projects from "./pages/Projects";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div>
+    <Router>
       <NavBar />
+      <Wrapper>
+        <Route exact path="/" component={About} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+      </Wrapper>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
