c4cf04606533d7c0e0553c8d70bf6b386c75b2dc
Wesley <wesaboo@gmail.com>
2020-09-15 21:39:40 -0700

    navigation and pages

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
+import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Projects from "./pages/Projects";
-import Wrapper from "./components/Wrapper";
 
 function App() {
 
-  //const [Nav, setNav] = useState("about");
-
   return (
-    <Router>
-        <NavBar />
-        <Wrapper>
-          <Route exact path="/" component={About}/>
-          <Route exact path="/about" component={About} />
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/contact" component={Contact} />
-        </Wrapper>
-        <Footer />
-    </Router>
+    <div>
+      <Navigation />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Navigation.js
@@ -0,0 +1,88 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import About from "../pages/About";
+import Projects from "../pages/Projects";
+import Contact from "../pages/Contact";
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography component={'span'}>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function SimpleTabs() {
+  const classes = useStyles();
+  const preventDefault = (event) => event.preventDefault();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="simple tabs example"
+        >
+          <Tab href="/about" onClick={preventDefault} label="About Me" {...a11yProps(0)} />
+
+          <Tab href="/projects" onClick={preventDefault} label="Projects" {...a11yProps(1)} />
+
+          <Tab href="/contact" onClick={preventDefault} label="Contact Me" {...a11yProps(2)} />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <About />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <Projects />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <Contact />
+      </TabPanel>
+    </div>
+  );
+}
