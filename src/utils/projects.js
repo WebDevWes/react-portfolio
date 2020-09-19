90ef10e8a70805fb5e518835015225ba8434fa57
Wesley <wesaboo@gmail.com>
2020-09-18 17:59:25 -0700

    got images to display

--- src/utils/projects.js
+++ src/utils/projects.js
@@ -2,7 +2,7 @@ const projects = [
     {
         "id": 1,
         "title": "Password Generator",
-        "src": "/images/PasswordGenerator.png",
+        "src": process.env.PUBLIC_URL+ "/images/PasswordGenerator.png",
         "description": "Generic Password Generator, generates randomize password based on selection of length, and type of characters.",
         "deployed_link": "https://wesaboo.github.io/Password-Generator/",
         "repo_link": "https://github.com/wesaboo/Password-Generator"
@@ -10,7 +10,7 @@ const projects = [
     {
         "id": 2,
         "title": "Quiz Game",
-        "src": "/images/QuizGame.png",
+        "src": process.env.PUBLIC_URL+ "/images/QuizGame.png",
         "description": "Javascript Quiz Game",
         "deployed_link": "https://wesaboo.github.io/QuizGame/",
         "repo_link": "https://github.com/wesaboo/QuizGame"
@@ -18,7 +18,7 @@ const projects = [
     {
         "id": 3,
         "title": "Day Planner",
-        "src": "/images/DayPlanner.png",
+        "src": process.env.PUBLIC_URL+ "/images/DayPlanner.png",
         "description": "Day Planner, plan your 9-5 Work Day!",
         "deployed_link": "https://wesaboo.github.io/DayPlanner/",
         "repo_link": "https://github.com/wesaboo/DayPlanner"
@@ -26,7 +26,7 @@ const projects = [
     {
         "id": 4,
         "title": "Weather Dashboard",
-        "src": "/images/WeatherDash.png",
+        "src": process.env.PUBLIC_URL+ "/images/WeatherDash.png",
         "description": "Get your weather information! Provides Daily and 5-day forecast",
         "deployed_link": "https://wesaboo.github.io/DayPlanner/",
         "repo_link": "https://github.com/wesaboo/DayPlanner"
@@ -34,7 +34,7 @@ const projects = [
     {
         "id": 5,
         "title": "Travel Nurses Job Spot",
-        "src": "/images/TravelNurse.png",
+        "src": process.env.PUBLIC_URL+ "/images/TravelNurse.png",
         "description": "Developed with the intention of helping Travel Nurses find the most financially viable job",
         "deployed_link": "https://staffbotsteve.github.io/lazy-loaders/",
         "repo_link": "https://github.com/wesaboo/lazy-loaders"
@@ -42,7 +42,7 @@ const projects = [
     {
         "id": 6,
         "title": "Note Taker",
-        "src": "/images/NoteTaker.png",
+        "src": process.env.PUBLIC_URL+ "/images/NoteTaker.png",
         "description": "Generic Note Taking web app",
         "deployed_link": "https://safe-caverns-86515.herokuapp.com/",
         "repo_link": "https://github.com/wesaboo/NoteTaker"
@@ -50,7 +50,7 @@ const projects = [
     {
         "id": 7,
         "title": "Eat-Da-Burger",
-        "src": "/images/NomBurgers.png",
+        "src": process.env.PUBLIC_URL+ "/images/NomBurgers.png",
         "description": "Useless App made to better learn MySQL",
         "deployed_link": "https://desolate-refuge-10655.herokuapp.com/",
         "repo_link": "https://github.com/wesaboo/Eat-Da-Burger"
@@ -58,7 +58,7 @@ const projects = [
     {
         "id": 8,
         "title": "Collector's Cache",
-        "src": "/images/CollectorsCache.png",
+        "src": process.env.PUBLIC_URL+ "/images/CollectorsCache.png",
         "description": "Web App to keep track of Card collection, and retrieve market values",
         "deployed_link": "https://immense-spire-70706.herokuapp.com/",
         "repo_link": "https://github.com/wesaboo/Project-2"
@@ -66,7 +66,7 @@ const projects = [
     {
         "id": 9,
         "title": "Fitness Tracker",
-        "src": "/images/FitnessTracker.png",
+        "src": process.env.PUBLIC_URL+ "/images/FitnessTracker.png",
         "description": "Web App to keep track your workout, and provides a table to track total weight lifted",
         "deployed_link": "https://still-lake-01483.herokuapp.com/",
         "repo_link": "https://github.com/wesaboo/Fitness-Tracker"
@@ -74,7 +74,7 @@ const projects = [
     {
         "id": 10,
         "title": "Budget Tracker",
-        "src": "/images/BudgetTracker.png",
+        "src": process.env.PUBLIC_URL+ "/images/BudgetTracker.png",
         "description": "Web App to keep track of your budget, and provides a graph to show visual representation of spending over time",
         "deployed_link": "https://secure-headland-85951.herokuapp.com/",
         "repo_link": "https://github.com/wesaboo/Budget-Tracker"
@@ -82,7 +82,7 @@ const projects = [
     {
         "id": 11,
         "title": "Employee Directory",
-        "src": "/images/EmployeeDirectory.png",
+        "src": process.env.PUBLIC_URL+ "/images/EmployeeDirectory.png",
         "description": "Dynamically generated page using React",
         "deployed_link": "https://wesaboo.github.io/employee-directory-react/",
         "repo_link": "https://github.com/wesaboo/employee-directory-react"
@@ -90,7 +90,7 @@ const projects = [
     {
         "id": 12,
         "title": "Google Books Search",
-        "src": "/images/GoogleBooks2.png",
+        "src": process.env.PUBLIC_URL+ "/images/GoogleBooks2.png",
         "description": "Find and store books onto MongoDB using Google Books API",
         "deployed_link": "https://secure-ravine-38448.herokuapp.com/",
         "repo_link": "https://github.com/wesaboo/google-books-search"
